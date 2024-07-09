49a169bcce2167a51dd5aaee9e6f81b60df3e8db
V11an <allankertich@gmail.com>
2024-07-09 21:17:33 +0300

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+import asyncio
+import random
+
+async def async_generator():
+  """
+  Async generator that yields random numbers 10 times with 1 second delay.
+  """
+  for _ in range(10):
+    await asyncio.sleep(1)
+    yield random.randint(0, 10)
