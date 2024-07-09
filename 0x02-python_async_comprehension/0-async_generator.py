a16e4feda12ec1b8a39351d296db9d2da4af2437
V11an <allankertich@gmail.com>
2024-07-09 21:25:36 +0300

    adding python documentation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,8 +5,12 @@ import random
 
 async def async_generator():
   """
-  Async generator that yields random numbers 10 times with 1 second delay.
+  Asynchronous generator that yields random numbers 10 times with 1 second delay.
+
+  Yields:
+      int: A random integer between 0 and 10 (inclusive).
   """
+
   for _ in range(10):
     await asyncio.sleep(1)
     yield random.randint(0, 10)
