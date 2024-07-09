cba5af4e7c2dd1e991340f335a4572824cd356e8
V11an <allankertich@gmail.com>
2024-07-09 21:28:47 +0300

    pycode style

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,13 +4,13 @@ import asyncio
 import random
 
 async def async_generator():
-  """
-  Asynchronous generator that yields random numbers 10 times with 1 second delay.
+    """
+    Asynchronous generator that yields random numbers 10 times with 1 second delay.
 
-  Yields:
-      int: A random integer between 0 and 10 (inclusive).
-  """
+    Yields:
+        int: A random integer between 0 and 10 (inclusive).
+    """
 
-  for _ in range(10):
-    await asyncio.sleep(1)
-    yield random.randint(0, 10)
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
